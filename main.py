8768a9787c87912ae11aa519ade0a80d252c0bed
Jared Ucherek <24571854+jareducherek@users.noreply.github.com>
2021-07-06 17:11:30 -0500

    add win32api

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import time
 import cv2
 import pyautogui
+import win32api, win32con
 import numpy as np
 import keyboard
 
-from target_finder import target_finder
+from target_finder import target_finder_frame_diff, target_finder
 
 #pause functions
 is_paused = True
 located = False
+
+def click(x, y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.06)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 if __name__ == "__main__":
     region = (1061, 447, 759, 533)
@@ -18,13 +25,16 @@ if __name__ == "__main__":
     while keyboard.is_pressed('s') == False:
         time.sleep(0.1)
 
+    print('starting!')
+    prev_frame = np.array(pyautogui.screenshot(region=region))
+    time.sleep(0.1)
     while keyboard.is_pressed('q') == False:
-        im = pyautogui.screenshot(region=region)
-        im = np.array(im)
-        pairs = target_finder(im)
+        cur_frame = np.array(pyautogui.screenshot(region=region))
+        pairs = target_finder(cur_frame)
         if pairs is not None:
             for pair in pairs:
-                print(pair)
                 x, y = pair
-                pyautogui.click(x=x+region[0], y=y+region[1])
+                click(x+region[0],y+region[1])
+                #pyautogui.click(x=x+region[0], y=y+region[1])
+        #prev_frame = cur_frame
 
